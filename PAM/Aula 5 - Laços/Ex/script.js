1771e62a96fad3931a50f2c54e62a3079bcecb00
ANDERSON REIS <137122362+EuAndersonDev@users.noreply.github.com>
2024-10-23 07:56:23 -0300

    Add files via upload

---
+++ PAM/Aula 5 - Laços/Ex/script.js
@@ -0,0 +1,20 @@
+
+function tabuada(multiplicado) {
+    let resultadoHTML = '';
+    for (var i = 0; i <= 10; i++) {
+        let resultado = multiplicado * i;
+        resultadoHTML += multiplicado + " x " + i + " = " + resultado + "<br>";
+    }
+    return resultadoHTML;
+}
+
+function gerarTabuada() {
+    const numero = document.getElementById('numero').value;
+    const resultadoDiv = document.getElementById('resultado');
+
+    if (numero === '') {
+        resultadoDiv.innerHTML = 'Por favor, digite um número!';
+    } else {
+        resultadoDiv.innerHTML = tabuada(parseInt(numero));
+    }
+}
